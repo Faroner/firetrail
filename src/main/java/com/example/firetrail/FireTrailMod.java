e13a7c6233aeb05cce32dd9d9b61d99108e4f8e3
Faroner <stanoffstan@gmail.com>
2026-09-22 21:03:02 +0300

    Add files via upload

--- src/main/java/com/example/firetrail/FireTrailMod.java
+++ src/main/java/com/example/firetrail/FireTrailMod.java
@@ -1,10 +1,8 @@
 package com.example.firetrail;
 
-import com.example.firetrail.registry.ModEntities;
-import com.example.firetrail.registry.ModSpells;
 import com.example.firetrail.network.ModNetwork;
+import com.example.firetrail.registry.ModSpells;
 import net.minecraftforge.eventbus.api.IEventBus;
-import software.bernie.geckolib.GeckoLib;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -13,10 +11,8 @@ public final class FireTrailMod {
     public static final String MODID = "firetrail";
 
     public FireTrailMod() {
-        GeckoLib.initialize();
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModSpells.SPELLS.register(bus);
         ModNetwork.register();
-        ModEntities.ENTITIES.register(bus);
     }
 }
--- src/main/java/com/example/firetrail/registry/ModSpells.java
+++ src/main/java/com/example/firetrail/registry/ModSpells.java
@@ -1,8 +1,6 @@
 package com.example.firetrail.registry;
 
 import com.example.firetrail.FireTrailMod;
-import com.example.firetrail.spell.FireTrailSpell;
-import com.example.firetrail.spell.HighFiveSpell;
 import com.example.firetrail.spell.DarkSpawnSpell;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
@@ -13,12 +11,6 @@ import java.util.function.Supplier;
 public final class ModSpells {
     public static final DeferredRegister<AbstractSpell> SPELLS =
             DeferredRegister.create(SpellRegistry.SPELL_REGISTRY_KEY, FireTrailMod.MODID);
-
-    public static final Supplier<AbstractSpell> FIRE_TRAIL =
-            SPELLS.register("fire_trail", FireTrailSpell::new);
-
-    public static final Supplier<AbstractSpell> HIGH_FIVE =
-            SPELLS.register("high_five", HighFiveSpell::new);
 
     public static final Supplier<AbstractSpell> DARK_SPAWN =
             SPELLS.register("dark_spawn", DarkSpawnSpell::new);
